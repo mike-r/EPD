f514efda0cd18fd5be5b9177d398f819fd6bfaa6
zap <zap@tr182.com>
2026-01-22 16:45:07 -0700

    reduced loop time

--- ow.py
+++ ow.py
@@ -91,4 +91,4 @@ while True:
             print("Unable to retrieve data at {}".format(url))
 
     gfx.update_time()
-    time.sleep(5)  # wait 5 seconds before updating anything again
+    time.sleep(0.1)  # wait 0.1 seconds before updating anything again
